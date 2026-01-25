db491835eb06e0ecad867370c9d8d92cc05635ac
Dinh Van Quan <quan0quan0quan@gmail.com>
2026-01-25 13:29:44 +0700

    fix docker cho producer

--- producer/taxi/taxi_producer.py
+++ producer/taxi/taxi_producer.py
@@ -1,8 +1,42 @@
 # kafka/producer/taxi_producer.py
 
 import csv
-from base_producer import RealtimeReplayProducer
-from config_producer import KAFKA_BOOTSTRAP_SERVERS, TAXI_TOPIC, SPEED_FACTOR
+import time
+from datetime import datetime
+from kafka import KafkaProducer
+import json
+
+KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
+TAXI_TOPIC = "nyc_taxi_trips"
+CSV_PATH = "../../datasets/2018_taxi_trips.csv"
+SPEED_FACTOR = 3600  # 1 hour data = 1 second realtime
+
+
+class RealtimeReplayProducer:
+    def __init__(self, topic, bootstrap_servers, speed_factor=3600):
+        self.topic = topic
+        self.speed_factor = speed_factor
+
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        )
+
+        self.prev_event_time = None
+
+    def parse_time(self, value):
+        return datetime.fromisoformat(value)
+
+    def send_event(self, event, event_time_field):
+        event_time = self.parse_time(event[event_time_field])
+
+        if self.prev_event_time:
+            delta = (event_time - self.prev_event_time).total_seconds()
+            if delta > 0:
+                time.sleep(delta / self.speed_factor)
+
+        self.producer.send(self.topic, value=event)
+        self.prev_event_time = event_time
 
 
 CSV_PATH = "../../../datasets/2018_taxi_trips.csv"
