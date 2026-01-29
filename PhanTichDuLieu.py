9df97c3330bf183b0ce36efa212bca9f52b7154a
doanvinhlong18 <doanvinhlong18@gmail.com>
2026-01-29 13:54:22 +0700

    update datasets

--- PhanTichDuLieu.py
+++ PhanTichDuLieu.py
@@ -1,43 +1,22 @@
 import pandas as pd
+import pyarrow.parquet as pq
 import os
+
+from fastparquet.api import row_groups_map
 
 file_path = "datasets/2018_taxi_trips.csv"
 
-size_mb = os.path.getsize(file_path) / 1024**2
-print(f"Dung lượng file trên disk: {size_mb:.2f} MB")
+for cnt in range(1, 12):
+    file_path = f"datasets/fhvhv_tripdata_2025-{cnt:02d}.parquet"
 
-# Chỉ đọc cấu trúc cột
-df_head = pd.read_csv(file_path, nrows=0)
+    pf = pq.ParquetFile(file_path)
 
-print("\nCÁC CỘT TRONG DATASET:")
-print(df_head.columns.tolist())
-print(f"Số cột: {len(df_head.columns)}")
-
-sample_df = pd.read_csv(file_path, nrows=10_000)
-
-
-print("\n" + "="*80)
-print("TỔNG QUAN TỪ MẪU 10,000 DÒNG")
-print("="*80)
-
-
-print(sample_df.head())
-print("\nShape mẫu:", sample_df.shape)
-
-print("\nKIỂU DỮ LIỆU:")
-print(sample_df.dtypes)
-
-numeric_cols = sample_df.select_dtypes(include=["int64", "float64"]).columns.tolist()
-categorical_cols = sample_df.select_dtypes(include=["object"]).columns.tolist()
-
-print("\nCột số:", numeric_cols)
-print("Cột phân loại:", categorical_cols)
-
-for col in sample_df.columns:
-    if "datetime" in col:
-        sample_df[col] = pd.to_datetime(sample_df[col], errors="coerce")
-
-print("\nSau khi parse datetime:")
-print(sample_df.dtypes)
+    print(f"tháng {cnt}:")
+    schema = pf.schema_arrow
+    print("schema: ")
+    print(schema)
+    metadata = pf.metadata
+    print("metadata: ")
+    print(metadata)
 
 
--- read_data.py
+++ read_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_parquet("datasets/fhvhv_tripdata_2025-02.parquet")
+df = pd.read_parquet("datasets/fhvhv_tripdata_2025-11.parquet")
 
 print(df.head())
 print(df.info())
